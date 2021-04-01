1ce8e87a2c11a55e83fe2c91d42b03e7db6ade52
JustinSingleton <jsingle9@msudenver.edu>
2021-03-31 22:09:18 -0600

    static files included

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ const port = 3000;
 const bodyParser = require('body-parser');
 const request = require('request');
 
+app.use(express.static('public'));
 
+app.get('/', (req, res)=>{
+  res.sendFile(__dirname + "/signup.html");
+});
 
 app.listen(port, ()=>{
   console.log("Running on port 3000");
