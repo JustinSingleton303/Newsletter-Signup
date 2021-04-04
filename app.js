427c758b117089a80d94647781b1c59df3b2fc20
JustinSingleton <jsingle9@msudenver.edu>
2021-04-03 23:27:15 -0600

    added mailchimp npm package

--- app.js
+++ app.js
@@ -5,11 +5,14 @@ const bodyParser = require('body-parser');
 const request = require('request');
 const https = require('https');
 const mailChimp = require("@mailchimp/mailchimp_marketing");
-
+const listID = '4f2529b3c7';
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
 
-
+mailChimp.setConfig({
+  apiKey: '13b4944c4bbc8078cd2f743eb9',
+  server: 'us1'
+});
 
 app.post('/', (req, res)=>{
   const fName = req.body.firstName;
@@ -33,11 +36,12 @@ app.post('/', (req, res)=>{
       }
     });
   }
-  const jsonData = JSON.stringify(data);
+  console.log('member add successful?');
+  /*const jsonData = JSON.stringify(data);
   const url = "https://$API_SERVER.api.mailchimp.com/3.0/lists/4f2529b3c7";
   https.request(url, options, (request)=>{
 
-  });
+  });*/run().catch(e => res.sendFile(__dirname + "/failure.html"));
 });
 
 app.get('/', (req, res)=>{
@@ -47,6 +51,8 @@ app.get('/', (req, res)=>{
 app.listen(port, ()=>{
   console.log("Running on port 3000");
 });
+
+
 
 //api key 13b4944c4bbc8078cd2f743eb9
 //list id 4f2529b3c7
